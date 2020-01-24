eec2619ab0fc5f9cbffb6beb01f684041d92db74
alideveloper <aly.dihaw96@gmail.com>
2020-01-24 22:20:28 +0700

    Import All Module & Style Standard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,44 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgxCarouselModule } from 'ngx-carousel';
+import 'hammerjs';
+
+import { NavbarModule } from './navbar/navbar.module';
+import { SlideModule } from './slide/slide.module';
+import { AboutUsModule } from './about-us/about-us.module';
+import { PartnerModule } from './partner/partner.module';
+import { FooterModule } from './footer/footer.module';
+
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+
+    NgbModule,
+
+    NavbarModule,
+    SlideModule,
+    AboutUsModule,
+    PartnerModule,
+    FooterModule,
+
+    AppRoutingModule,
+    FontAwesomeModule,
+    NgxCarouselModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: HammerGestureConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
